abd2fb19148c2f8de03f4f1b1608e38ea54f0d34
Tatenda Manyepa <105117513+Tayte23@users.noreply.github.com>
2025-10-03 14:02:09 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/NewLeafOasis/"
-});
+  base: '/NewLeafOasis/',          // 👈 your repo name
+  build: { outDir: 'docs' }        // GH Pages can deploy from /docs on main
+})
+
