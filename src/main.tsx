b903a676116e740a62aec6fe1e4cc85ca6d57693
Tatenda Manyepa <105117513+Tayte23@users.noreply.github.com>
2025-10-03 20:08:40 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import NewLeafOasis from "./NewLeafOasis";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NewLeafOasis />
   </React.StrictMode>
