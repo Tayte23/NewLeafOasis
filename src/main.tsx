7c42a680ca3d6e5cf43fa3a72975c70a8aa82d9c
Tatenda Manyepa <105117513+Tayte23@users.noreply.github.com>
2025-10-03 19:27:15 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import NewLeafOasis from "./NewLeafOasis";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename="/">
-      <NewLeafOasis />
-    </HashRouter>
+    <NewLeafOasis />
   </React.StrictMode>
 );
