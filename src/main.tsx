713bded5d10b7f082a2953ba7ff49a05f46c9519
Tatenda Manyepa <105117513+Tayte23@users.noreply.github.com>
2025-10-03 14:29:02 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import NewLeafOasis from "./NewLeafOasis";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './NewLeafOasis' // or your root component
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <NewLeafOasis />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter basename="/NewLeafOasis">
+    <App />
+  </BrowserRouter>
+)
