852c70b8135a3f5b2937e045c0cae7ee698091be
Tatenda Manyepa <105117513+Tayte23@users.noreply.github.com>
2025-11-16 01:50:44 +0000

    Delete src/main.tsx

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import NewLeafOasis from "./NewLeafOasis";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <NewLeafOasis />
-  </React.StrictMode>
-);
