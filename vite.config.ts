1664928e84dc3a96b5ae67d0742212486421153b
Tatenda Manyepa <105117513+Tayte23@users.noreply.github.com>
2025-11-16 01:50:10 +0000

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/NewLeafOasis/",   // <- your repo name
-  build: { outDir: "dist" } // <- matches the Pages workflow
-});
